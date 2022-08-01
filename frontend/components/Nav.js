ee6269beb5b7c8c75786e03af3e9f89b3b207832
Cristina Lester <cristina.lester@activision.com>
2022-08-01 15:43:50 -0700

    added getInitialProps to _document

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,10 +4,10 @@ import React from 'react';
 function Nav() {
   return (
     <nav>
-      <Link href="products">Products</Link>
-      <Link href="sell">Sell</Link>
-      <Link href="orders">Orders</Link>
-      <Link href="account">Account</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/sell">Sell</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
     </nav>
   );
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routerChangeComplete', () => NProgress.done());
+Router.events.on('routerChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
